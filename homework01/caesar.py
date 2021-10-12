9c634fda25acf02e89969b5008d378e8f5c9052c
LizaArnautova <Liza.arnautova@gmail.com>
2021-10-12 15:37:43 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,45 +2,41 @@ import typing as tp
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+    if len(plaintext) == 0:
+        return plaintext
+    list = []
+    for i in range(len(plaintext)):
+        a = ord(plaintext[i]) + shift
+        if (65 <= a <= 90) or (97 <= a <= 122):
+            list.append(a)
+        elif 32 <= a <= 64:
+            list.append(a - shift)
+        else:
+            list.append(a - 26)
+    ciphertext = ''.join([chr(i) for i in list])
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    if len(ciphertext) == 0:
+        return ciphertext
+    list = []
+    for i in range(len(ciphertext)):
+        a = ord(ciphertext[i]) - shift
+        if (65 <= a <= 90) or (97 <= a <= 122):
+            list.append(a)
+        elif 32 <= a <= 61:
+            list.append(a + shift)
+        else:
+            list.append(a + 26)
+    plaintext = ''.join([chr(i) for i in list])
     return plaintext
 
 
+
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
     best_shift = 0
-    # PUT YOUR CODE HERE
     return best_shift
+
+print(decrypt_caesar("SBWKRQ", 3))
+print(encrypt_caesar("PYTHON", 3))
