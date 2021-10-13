ede561ba9bb1a1d2f0ef0828471d90f6b44ae37d
LizaArnautova <Liza.arnautova@gmail.com>
2021-10-14 00:42:18 +0300

    Реализована функция encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,30 +1,59 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
+    if len(plaintext) == 0:
+        return plaintext
+    while len(keyword) < len(plaintext):
+        keyword += keyword
+    key = []
+    for i in range(len(plaintext)):
+        a = ord(keyword[i]) - 97 if ord(keyword[i]) - 65 > 25 else ord(keyword[i]) - 65
+        key.append(a)
 
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+    notupp = plaintext.islower()
+    if notupp == 1:
+        plaintext = plaintext.upper()
+
+    list = []
+    for i in range(len(plaintext)):
+        a = ord(plaintext[i]) + key[i]
+        if 65 <= a <= 90:
+            list.append(a)
+        elif 32 <= a <= 64:
+            list.append(a - key[i])
+        else:
+            list.append(a - 26)
+
+    ciphertext = "".join([chr(i) for i in list])
+    if notupp == 1:
+        ciphertext = ciphertext.lower()
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
+    if len(ciphertext) == 0:
+        return ciphertext
+    while len(keyword) < len(ciphertext):
+        keyword += keyword
+    key = []
+    for i in range(len(ciphertext)):
+        a = ord(keyword[i]) - 97 if ord(keyword[i]) - 65 > 25 else ord(keyword[i]) - 65
+        key.append(a)
 
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    notupp = ciphertext.islower()
+    if notupp == 1:
+        ciphertext = ciphertext.upper()
+
+    list = []
+    for i in range(len(ciphertext)):
+        a = ord(ciphertext[i]) - key[i]
+        if 65 <= a <= 90:
+            list.append(a)
+        elif 32 <= a <= 64:
+            list.append(a + 26)
+        else:
+            list.append(a + 26)
+
+    plaintext = "".join([chr(i) for i in list])
+    if notupp == 1:
+        plaintext = plaintext.lower()
     return plaintext
